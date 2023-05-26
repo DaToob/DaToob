e96a227ab35f96448581537924df7cedbd9a030d
ljcool2006 <51834343+ljcool2006@users.noreply.github.com>
2023-05-26 07:33:46 -0400

    Update profile.php

--- public/profile.php
+++ public/profile.php
@@ -26,9 +26,9 @@ namespace rePok {
         $age = false;
     }
     
-    if ($sql->result("SELECT * FROM friends WHERE (sender = ? AND receiver = ?) AND status = 0", [$userdata['id'], $userpagedata['id']])) {
+    if ($sql->result("SELECT * FROM friends WHERE (sender = ? AND receiver = ?) OR (sender = ? AND receiver = ?) AND status = 0", [$userdata['id'], $userpagedata['id'], $userpagedata['id'], $userdata['id']])) {
         $friendState = 1;
-    } elseif ($sql->result("SELECT * FROM friends WHERE (sender = ? AND receiver = ?) AND status = 1", [$userdata['id'], $userpagedata['id']])) {
+    } elseif ($sql->result("SELECT * FROM friends WHERE (sender = ? AND receiver = ?) OR (sender = ? AND receiver = ?) AND status = 1", [$userdata['id'], $userpagedata['id'], $userpagedata['id'], $userdata['id']])) {
         $friendState = 2;
     } else {
         $friendState = 0;
