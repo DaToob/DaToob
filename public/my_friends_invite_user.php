262717bb280a227132449b54cf585fca375e0021
ljcool2006 <51834343+ljcool2006@users.noreply.github.com>
2023-05-26 06:49:58 -0400

    Update my_friends_invite_user.php

--- public/my_friends_invite_user.php
+++ public/my_friends_invite_user.php
@@ -4,6 +4,7 @@ namespace rePok {
 
     require_once dirname(__DIR__) . '/private/class/common.php';
 
+    if (!$log) redirect('login.php');
     if (isset($_GET['friend_id'])) {
         Friend::sendRequest($userdata['id'], $_GET['friend_id']);
         closeWindow();
